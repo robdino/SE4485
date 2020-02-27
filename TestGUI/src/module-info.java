9c076feb59ede7efa42192e6f903caf9d2cf61e2
zhusain <47133159+zhusain@users.noreply.github.com>
2020-02-26 18:48:07 -0600

    Initial upload of GUI

---
+++ TestGUI/src/module-info.java
@@ -0,0 +1,6 @@
+module TestGUI {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
---
+++ TestGUI/src/sample/Controller.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Controller {
+    public void SearchButtonClicked(){
+        System.out.println("Searching for customer...");
+    }
+    public void setCreditRange(){
+
+    }
+}
