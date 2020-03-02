9db6a030ff00c94ecda93b816b4c05c438d44067
zhusain <47133159+zhusain@users.noreply.github.com>
2020-03-02 14:43:45 -0600

    Update Controller.java

--- TestGUI/src/sample/Controller.java
+++ TestGUI/src/sample/Controller.java
@@ -1,10 +1,26 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+
 public class Controller {
-    public void SearchButtonClicked(){
-        System.out.println("Searching for customer...");
+    public TextField SS_Number = new TextField();
+
+//    public void setSS_Number(String SS_Number) {
+//        this.SS_Number.setOnAction(Key);
+//    }
+
+    public String getSS_Number() {
+        return SS_Number.getText();
     }
-    public void setCreditRange(){
+
+    public String first;
+    int cRangeLow, cRangeHigh;
+    public void SearchButtonClicked(){
+        System.out.println("SS Number: " + getSS_Number());
 
     }
+
+
 }
