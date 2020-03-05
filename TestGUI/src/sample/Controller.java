6dc6e80b47d2fcf7444f990486c5d633e18ef562
Zain <zain.g.husain@gmail.com>
2020-03-05 00:36:12 -0600

    Added options to combo box and ability to retrieve selection

--- TestGUI/src/sample/Controller.java
+++ TestGUI/src/sample/Controller.java
@@ -1,6 +1,9 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
 import javafx.event.EventHandler.*;
@@ -9,6 +12,15 @@ import javafx.event.ActionEvent;
 
 public class Controller {
     public TextField SS_Number = new TextField();
+    public TextField name = new TextField();
+    public TextField credit_High = new TextField();
+    public TextField credit_Low = new TextField();
+    public TextField city = new TextField();
+    public ComboBox state = new ComboBox();
+
+
+
+
 
 //    public void setSS_Number(String SS_Number) {
 //        this.SS_Number.setOnAction(Key);
@@ -18,10 +30,15 @@ public class Controller {
         return SS_Number.getText();
     }
 
+    public String getState() {
+        return state.getSelectionModel().getSelectedItem().toString();
+    }
+
     public String first;
     int cRangeLow, cRangeHigh;
     public void SearchButtonClicked(){
         System.out.println("SS Number: " + getSS_Number());
+        System.out.println("State: " + getState());
        // System.out.println("State: " + getState());
     }
     
