ea0f9993d426719636415a014964884c9984c610
Zain <zain.g.husain@gmail.com>
2020-03-05 01:05:24 -0600

    removed some unneeded imports and javafx set back to application

--- TestGUI/src/sample/Main.java
+++ TestGUI/src/sample/Main.java
@@ -5,20 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+
 
 public class Main extends Application {
-    Label userSelectionMsgLbl = new Label("Your selection: ");
-    Label userSelectionDataLbl = new Label("");
-    Label itemChangeLbl = new Label("Item Changed: ");
-    Label indexChangeLbl = new Label("Index Changed: ");
 
     @Override
     public void start(Stage primaryStage) throws Exception{
